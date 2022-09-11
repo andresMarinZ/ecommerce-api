2cf3efe22ad46b3b7aab18b0a3e858d3d768e2a5
Andres <andres_marin1992@hotmail.com>
2022-09-11 14:37:23 -0500

    Create structure project

---
+++ src/test/java/com/acs/ecommerce/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acs.ecommerce.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
