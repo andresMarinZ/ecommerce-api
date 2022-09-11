76abe8e75b31fce9480e3b85b6a13b17c927fbcc
DOMAIN\jhon.pavon <jpavon@intergrupo.com>
2022-09-11 15:35:38 -0500

    add enpoint post and put

--- src/main/java/com/acs/ecommerce/api/controller/ReviewController.java
+++ src/main/java/com/acs/ecommerce/api/controller/ReviewController.java
@@ -1,4 +1,47 @@
 package com.acs.ecommerce.api.controller;
 
+import com.acs.ecommerce.api.model.genericModel.Response;
+import com.acs.ecommerce.api.service.iservice.IReviewService;
+import com.acs.ecommerce.api.model.ReviewModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class ReviewController {
+    private final List<ReviewModel> reviewList = new ArrayList<>();
+
+    /*private IReviewService _IReviewService;
+    @Autowired
+    public ReviewController(IReviewService IReviewService){
+        _IReviewService = IReviewService;
+    }*/
+
+    @PostMapping("/review")
+    public ResponseEntity<Response<ReviewModel>> save(@RequestBody ReviewModel review) {
+
+        var responseEntity = new Response<ReviewModel>();
+        responseEntity.setMessage("created success");
+        responseEntity.setStatus(true);
+        responseEntity.setData(review);
+        return new ResponseEntity<>(responseEntity, null, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/review")
+    public ResponseEntity<Response<ReviewModel>> update(@RequestBody ReviewModel review) {
+        var responseEntity = new Response<ReviewModel>();
+
+        responseEntity.setMessage("updated success");
+        responseEntity.setStatus(true);
+        responseEntity.setData(review);
+
+        return new ResponseEntity<>(responseEntity, null, HttpStatus.CREATED);
+    }
+
 }
