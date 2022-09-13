41e5f6d9f4329b745b3fe6edaada1fbf99759676
Juan Esteban Yepes <juan.yepesq@comunidad.iush.edu.co>
2022-09-12 22:59:39 -0500

    contenido que no se subio bien

--- src/main/java/com/acs/ecommerce/api/controller/securityController.java
+++ src/main/java/com/acs/ecommerce/api/controller/securityController.java
@@ -1,2 +1,59 @@
-package com.acs.ecommerce.api.controller;public class securityController {
+package com.acs.ecommerce.api.controller;
+
+import com.acs.ecommerce.api.model.security;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class securityController {
+    private final static List<security> list = new ArrayList<>();
+
+    @GetMapping("/security")
+    public List<security> getsecurity() {
+
+        ArrayList<security> list = new ArrayList<security>();
+        security first = new security();
+        first.setId(1);
+        first.setUserType("vendedor");
+        first.setFirstName("peranito");
+        first.setLastName("perez");
+        first.setDocumentType("cedula de ciudadania");
+        first.setDocumentNumber(1234567899);
+        first.setStoreName("ecommerce");
+
+        list.add(first);
+        return list;
+    }
+
+    @PostMapping("/security")
+    public String create( @RequestBody security request) {
+        return String.format("POST Recibido: \nAId: %s," +
+                        " \nusertype: %s," +
+                        " \nfirstname: %s," +
+                        " \nlastname: %s," +
+                        " \ndocumentType: %s," +
+                        " \ndocumentnumber: %s," +
+                        " \nstorename: %s ",
+                request.getId(),
+                request.getUserType(),
+                request.getFirstName(),
+                request.getLastName(),
+                request.getDocumentType(),
+                request.getDocumentNumber(),
+                request.getStoreName());
+    }
+
+    @PutMapping("/updatesecurity")
+    public security update(@RequestBody security request){
+        security response = request;
+
+        return response;
+    }
+
+    @DeleteMapping("/deletesecurity")
+    public String delete(@PathVariable int id){
+        return "ok";
+    }
 }
+
--- src/main/java/com/acs/ecommerce/api/model/security.java
+++ src/main/java/com/acs/ecommerce/api/model/security.java
@@ -1,2 +1,82 @@
-package com.acs.ecommerce.api.model;public class security {
+package com.acs.ecommerce.api.model;
+
+public class security {
+    private int id;
+    private String userType;
+    private String firstName;
+    private String lastName;
+    private String documentType;
+    private int documentNumber;
+    private String StoreName;
+
+    public security() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDocumentType() {
+        return documentType;
+    }
+
+    public void setDocumentType(String documentType) {
+        this.documentType = documentType;
+    }
+
+    public int getDocumentNumber() {
+        return documentNumber;
+    }
+
+    public void setDocumentNumber(int documentNumber) {
+        this.documentNumber = documentNumber;
+    }
+
+    public String getStoreName() {
+        return StoreName;
+    }
+
+    public void setStoreName(String storeName) {
+        StoreName = storeName;
+    }
+
+    public security(int id, String userType, String firstName, String lastName, String documentType, int documentNumber, String storeName) {
+        this.id = id;
+        this.userType = userType;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.documentType = documentType;
+        this.documentNumber = documentNumber;
+        StoreName = storeName;
+    }
 }
+
