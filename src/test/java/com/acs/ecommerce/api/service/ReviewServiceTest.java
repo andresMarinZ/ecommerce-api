56024bb99fa36a1e1845d6908a6640c5122a898a
Andres <andres_marin1992@hotmail.com>
2022-09-19 21:02:38 -0500

    Unit test method Delete

--- src/test/java/com/acs/ecommerce/api/service/ReviewServiceTest.java
+++ src/test/java/com/acs/ecommerce/api/service/ReviewServiceTest.java
@@ -8,7 +8,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,6 +28,18 @@ class ReviewServiceTest {
         reviewMockList.clear();
     }
 
+    private ReviewModel setReviewModel(Boolean viewed, String reviewId){
+
+        ReviewModel reviewModel = new ReviewModel();
+        reviewModel.setId(reviewId);
+        reviewModel.setCreatedAt(new Date(System.currentTimeMillis()));
+        reviewModel.setProductId("1234");
+        reviewModel.setViewed(viewed);
+
+        return reviewModel;
+
+    };
+
     @Test
     public void getAllReviewsWhenIsEmptyThenExpectsTrue(){
         //Arrange
@@ -34,6 +48,45 @@ class ReviewServiceTest {
 
         //Assert
         Assertions.assertTrue(reviews.isEmpty());
+    }
+
+    @Test
+    public void deleteReviewWhenReviewNotFoundThenExpectFalse(){
+        //Arrange
+        ReviewModel reviewModel = setReviewModel(false,"");
+        reviewMockList.add(reviewModel);
+        //Act
+
+        var reviewDelete = reviewServices.delete("123");
+
+        //Assert
+        Assertions.assertFalse(reviewDelete);
+    }
+
+    @Test
+    public void deleteReviewWhenReviewViewedThenExpectFalse(){
+        //Arrange
+        ReviewModel reviewModel = setReviewModel(true,"123");
+        reviewMockList.add(reviewModel);
+        //Act
+
+        var reviewDelete = reviewServices.delete("123");
+
+        //Assert
+        Assertions.assertFalse(reviewDelete);
+    }
+
+    @Test
+    public void deleteReviewThenExpectTrue(){
+        //Arrange
+        ReviewModel reviewModel = setReviewModel(false,"123");
+        reviewMockList.add(reviewModel);
+        //Act
+
+        var reviewDelete = reviewServices.delete("123");
+
+        //Assert
+        Assertions.assertTrue(reviewDelete);
     }
 
     @AfterEach
