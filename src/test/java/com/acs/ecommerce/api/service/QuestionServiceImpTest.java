3c555cc5bd32192f94d936f3babc79607972d5c9
Felipe Ortiz <felipe.ortiz.bedoya@gmail.com>
2022-09-23 20:57:45 -0500

    implementation of unit test

--- src/test/java/com/acs/ecommerce/api/service/QuestionServiceImpTest.java
+++ src/test/java/com/acs/ecommerce/api/service/QuestionServiceImpTest.java
@@ -1,13 +1,30 @@
 package com.acs.ecommerce.api.service;
 
+import com.acs.ecommerce.api.model.Question;
+import com.acs.ecommerce.api.service.iservice.IProductService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@ExtendWith(SpringExtension.class)
 class QuestionServiceImpTest {
+    private final List<Question> questiomocklist = new ArrayList<>();
+    private final List<Question> productmocklist = new ArrayList<>();
+    private final QuestionServiceImp questionmock;
+    private final IProductService iProductService;
+    public QuestionServiceImpTest(){
+        this.questionmock = new QuestionServiceImp(questiomocklist);
+        this.iProductService = new ProductService(productmocklist);
+    }
+
 
     @Test
     void create() {
+        questionmock.set
     }
 
     @Test
