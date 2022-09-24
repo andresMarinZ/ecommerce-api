c8e346b4d1a4367147254e878a33905d80e7a510
Joseph Mateus Raigoza Mesa <josephmrm2001@hotmail.com>
2022-09-23 22:32:37 -0500

    update unit test

--- src/test/java/com/acs/ecommerce/api/service/QuestionServiceImpTest.java
+++ src/test/java/com/acs/ecommerce/api/service/QuestionServiceImpTest.java
@@ -1,6 +1,8 @@
 package com.acs.ecommerce.api.service;
 
+import com.acs.ecommerce.api.model.ProductModel;
 import com.acs.ecommerce.api.model.Question;
+import com.acs.ecommerce.api.model.Response;
 import com.acs.ecommerce.api.service.iservice.IProductService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,24 +12,48 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 @ExtendWith(SpringExtension.class)
 class QuestionServiceImpTest {
     private final List<Question> questiomocklist = new ArrayList<>();
-    private final List<Question> productmocklist = new ArrayList<>();
-    private final QuestionServiceImp questionmock;
+    private final List<ProductModel> productmocklist = new ArrayList<>();
+    private final QuestionService questionMockService;
     private final IProductService iProductService;
-    public QuestionServiceImpTest(){
-        this.questionmock = new QuestionServiceImp(questiomocklist);
+
+    public QuestionServiceImpTest() {
         this.iProductService = new ProductService(productmocklist);
+        this.questionMockService = new QuestionServiceImp(iProductService);
     }
 
+    private Question getNewQuestion(String questionText, String buyerId, String sellerId, String productId) {
+        Question question = new Question();
+        question.setQuestionText(questionText);
+        question.setBuyerId(buyerId);
+        question.setSellerId(sellerId);
+        question.setProductId(productId);
+        return question;
+    }
 
     @Test
-    void create() {
-        questionmock.set
+    public void createWhenTextIsGreaterThan1000() {
+        final String questionText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. " +
+                "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, " +
+                "when an unknown printer took a galley of type and scrambled it to make a type specimen book. " +
+                "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. " +
+                "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, " +
+                "and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." +
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. " +
+                "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. " +
+                "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. " +
+                "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, " +
+                "and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+        Response res = questionMockService.create(getNewQuestion(questionText, "", "", ""));
+        System.out.println(res);
     }
 
     @Test
-    void getAll() {
+    public void getAll() {
     }
+
+
 }
